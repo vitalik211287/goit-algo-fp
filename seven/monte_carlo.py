2296d75191c520ab239fc1bc4e7cd0b0db17c59e
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-06-29 20:14:02 +0300

    Update monte_carlo.py

--- seven/monte_carlo.py
+++ seven/monte_carlo.py
@@ -1,5 +1,6 @@
 import random
-import pandas as pd
+# import pandas as pd
+import matplotlib.pyplot as plt
 
 def monte_carlo_simulation(throws=10000):
     counts = {i: 0 for i in range(2, 13)}
@@ -8,6 +9,20 @@ def monte_carlo_simulation(throws=10000):
         counts[dice_sum] += 1
     probabilities = {k: v / throws for k, v in counts.items()}
     return probabilities
+
+
+def plot_probabilities(probabilities):
+    plt.bar(probabilities.keys(), probabilities.values(), color='skyblue')
+    plt.title("Monte Carlo Dice Sum Probabilities")
+    plt.xlabel("Sum")
+    plt.ylabel("Probability")
+    plt.grid(True)
+    plt.show()
+
+# Виклик функції після симуляції
+probs = monte_carlo_simulation(5000)
+plot_probabilities(probs)
+
 
 print("\n=== Monte Carlo Dice ===")
 probs = monte_carlo_simulation(5000)
